5d717ddbd9ddd0b03cd66312d712541c1969b289
Brijeshthummar02 <brijeshthummar02@gmail.com>
2025-02-05 21:56:53 +0530

    singletone

---
+++ src/OOPS/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package OOPS.singleton;
+
+public class Singleton {
+   private Singleton (){
+
+    }
+
+    private static Singleton instance;
+
+   public static Singleton getInstance(){
+     // check whether 1 obj is created or not
+       if(instance == null){
+           instance = new Singleton();
+       } return instance;
+   }
+}
