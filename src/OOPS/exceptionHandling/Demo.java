66543619fba3dff8d511761ccc6bb8122764be16
Brijeshthummar02 <brijeshthummar02@gmail.com>
2025-02-10 21:08:08 +0530

    exception handling

---
+++ src/OOPS/exceptionHandling/Demo.java
@@ -0,0 +1,7 @@
+package OOPS.exceptionHandling;
+
+public class Demo {
+    public static void main(String[] args) {
+        Main.divide(3,4);
+    }
+}
---
+++ src/OOPS/exceptionHandling/Main.java
@@ -0,0 +1,33 @@
+package OOPS.exceptionHandling;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        try {
+//            divide(a, b);
+            // mimicing
+            String name = "brijesh";
+            if (name.equals("brijesh")) {
+                throw new MyException("name is brijesh");
+            }
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("normal exception");
+        } finally {
+            System.out.println("this will always execute");
+        }
+
+    }
+
+    static int divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("please do no divide by zero");
+        }
+
+        return a / b;
+    }
+}
