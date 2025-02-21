5893ae55a35440429e1bf5f34103f35e92f0dbe2
brijeshthummar02@gmail.com <brijeshthummar02@gmail.com>
2025-02-21 20:56:29 +0530

    Linked List

---
+++ src/Linkedlist/Main.java
@@ -0,0 +1,21 @@
+package Linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(1);
+        list.insertFirst(24);
+        list.insertFirst(4);
+        list.insertLast(90);
+       // list.insert(100, 2);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(1));
+        list.display();
+    }
+}
